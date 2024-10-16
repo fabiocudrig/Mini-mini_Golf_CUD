b21b359c353fa4fbfdbd02598afb6f0c072b863c
dam2 <dam2@a223e17.dc.domenicoscarlatti.es>
2024-10-16 17:45:09 +0200

    Creación del proyecto. Primer label.

---
+++ src/mini/mini_golf_cud/laControlador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mini.mini_golf_cud;
+
+/**
+ *
+ * @author dam2
+ */
+public class laControlador {
+    
+}
